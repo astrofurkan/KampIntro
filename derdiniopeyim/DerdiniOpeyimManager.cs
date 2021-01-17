e2590e1655a12ab8c4da496212c16575096a3c82
furkanakbaba <furkanakbaba34@outlook.com>
2021-01-18 01:35:18 +0300

    Engin Hocanın canını sıkan arkadaşa hitaben, şakasına beya :))

---
+++ derdiniopeyim/DerdiniOpeyimManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace derdiniopeyim
+{
+    class DerdiniOpeyimManager
+    {
+        public void DerdiniOpeyimButonu(Dert dert)
+        {
+            Console.WriteLine(" Tebrikler! Derdiniz Öpüldü : " + dert.Adi);
+
+        }
+    }
+}
---
+++ derdiniopeyim/Dert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace derdiniopeyim
+{
+    class Dert
+    {
+     
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
